f597ffc493df8a997e19a2de391a61a1fd1fb9ab
Konstantin Zamyakin <sc@adguard.com>
2017-08-29 14:34:51 +0300

    restore master

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -4,10 +4,11 @@ import Translator from 'bazinga-translator';
 export let translator;
 export let onTranslationLoad = [];
 
-let xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 xhr.open('GET', window.translations_url);
 xhr.onload = () => {
     if (xhr.status >= 200 && xhr.status < 300) {
+        Translator.locale = window.translation_locale;
         translator = Translator.fromJSON(xhr.response);
         onTranslationLoad.forEach((cb) => { cb(); });
     }
