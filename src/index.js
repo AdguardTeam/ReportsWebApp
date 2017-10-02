88e9f2380ac54419c57d2cdf6b78ec8cbe486477
Konstantin Zamyakin <sc@adguard.com>
2017-10-02 16:16:45 +0300

    Merge pull request #4 in ADGUARD/report-webapp from ADG-536-1 to master
    Squashed commit of the following:
    
    commit aab651ad03aeba66db6d9cc86aca1ce37958f20c
    Author: Ildar Kamalov <i.kamalov@adguard.com>
    Date:   Mon Oct 2 16:15:13 2017 +0300
    
        ADG-536 add raven

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { Provider } from 'react-redux';
 import App from './components/App.js';
 import store from './reducers';
 import { onTranslationLoad } from './constants/strings';
+import Raven from 'raven-js';
+
+if (window.sentry_client_key) {
+    Raven
+        .config(window.sentry_client_key)
+        .install()
+    ;
+}
 
 const render = () => {
     ReactDOM.render(
